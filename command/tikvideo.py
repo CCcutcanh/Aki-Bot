3369d16c1c973c9808944369208a266f30def66c
CCcutcanh <91305214+CCcutcanh@users.noreply.github.com>
2023-01-25 21:44:59 +0700

    Update tikvideo.py

--- command/tikvideo.py
+++ command/tikvideo.py
@@ -12,17 +12,16 @@ class Tikvideo(commands.Cog):
         self.bot = bot
     @commands.command()
     async def tikvideo(self, ctx,link_video_tiktok:str):
+        load_msg = await ctx.reply("Đang thực hiện yêu cầu vui lòng đợi...")
         try:
-            params = {
-                'link': link_video_tiktok
-            }
             async with aiohttp.ClientSession() as session:
-                async with session.get('https://docs-api.nguyenhaidang.ml/tiktok/download?', params=params) as get:
-                    video_url_nowatermark = (await get.json())['play']
-    
+                url = f'https://www.nguyenmanh.name.vn/api/tikDL?url={link_video_tiktok}&apikey=3AHlbNbA'
+                async with session.get(url) as get:
+                    video_url_nowatermark = (await get.json())['result']["video"]["nowatermark"]
+                    print(url)
                 async with session.get(f'{video_url_nowatermark}') as get2:
                     video = await get2.read()
-                
+                await load_msg.delete()
                 if len(video) > 1e9: 
                     await ctx.reply(content='Video tiktok quá dài không thể gửi!', ephemeral=False)
                     return
