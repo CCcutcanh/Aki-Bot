d56dfae99e569a3b5dde28aaa6e5f2c37acfa548
CCcutcanh <91305214+CCcutcanh@users.noreply.github.com>
2023-01-25 11:39:54 +0700

    Update dhbc.py

--- command/dhbc.py
+++ command/dhbc.py
@@ -25,92 +25,85 @@ class Dhbc(commands.Cog):
         try:
             self.bot.sql.execute(f'SELECT user_money FROM user_data WHERE user_id={ctx.author.id}')
             money = self.bot.sql.fetchone()[0]
-            async with aiohttp.ClientSession() as session:
-                api = ['https://api.phamvandien.xyz/game/dhbcv1', 'https://www.nguyenmanh.name.vn/api/dhbc1?apikey=KCL98tNB', 'https://docs-api.nguyenhaidang.ml/game/dhbc']
-                get = random.choice(api)
-                question = await session.get(get)
-                question = json.loads(await question.text())
-                if get == api[0]:
-                    get = await session.get(question["dataGame"]['link'])
-                    em = discord.Embed(title="_==== Đuổi Hình Bắt Chữ====_", description='đây là câu hỏi của bạn:\nreply tin nhắn này để trả lời câu hỏi và reply "gợi ý" để lấy gợi ý câu trả lời (50$/lần)', color = random.choice(list_color))
-                    file = discord.File(io.BytesIO(await get.read()), filename="dhbc.png")
-                    em.set_image(url = "attachment://dhbc.png")
-                    send = await ctx.send(embed=em, file = file)
-                    def check(m):
-                        return m.author == ctx.author and m.channel == ctx.channel and m.reference is not None and m.reference.message_id == send.id
-                    message = await self.bot.wait_for('message', check = check, timeout=45)
-                    if str(message.content).lower() == "gợi ý":
-                        self.bot.sql.execute(f'SELECT user_money FROM user_data WHERE user_id={ctx.author.id}')
-                        money = self.bot.sql.fetchone()[0]
-                        if int(money) < 50:
-                            await ctx.send('bạn không đủ 50$ trong ví để xem gợi ý')
-                            message = await self.bot.wait_for('message', check = check, timeout=45)
-                            if str(message.content).lower() == str(question['dataGame']['tukhoa']).lower():
-                                await ctx.send(f'bạn đã trả lời đúng, đáp án là: "{question["dataGame"]["tukhoa"]}"')
-                            else:
-                                await ctx.send(f"sai rồi:(, đáp án đúng là: {question['dataGame']['tukhoa']}")
-                        else:
+            self.bot.sql.execute(f'SELECT user_bank FROM user_data WHERE user_id={ctx.author.id}')
+            bank = self.bot.sql.fetchone()[0]
+            async def wait_message(msg):
+                def check(m):
+                    return m.author.id == ctx.author.id and m.channel == ctx.channel and m.reference is not None and m.reference.message_id == send.id
+                message = await self.bot.wait_for('message', check=check)
+                return message
+            url_DHBC = [
+                'https://www.nguyenmanh.name.vn/api/dhbc1?apikey=KCL98tNB',
+                "https://goatbotserver.onrender.com/api/duoihinhbatchu"
+            ]
+            random_dhbc = random.choice(url_DHBC)
+            get_DHBC = requests.get(random_dhbc)
+            data_DHBC = get_DHBC.text
+            json_DHBC = json.loads(data_DHBC)
+            embed = discord.Embed(colour = random.choice(list_color), title = "===ĐUỔI HÌNH BẮT CHỮ===", description = 'đây là câu hỏi của bạn\nreply "gợi ý" nếu bạn muốn xem gợi ý cho câu hỏi(50$/lần)')
+            if random_dhbc == url_DHBC[0]:
+                image_DHBC = json_DHBC['result']['link']
+                sokt = json_DHBC['result']['sokitu']
+                dapan = json_DHBC['result']['tukhoa']
+                get_image_DHBC = requests.get(image_DHBC)
+                file = open("dhbc.png", "wb")
+                file.write(get_image_DHBC.content)
+                file.close()
+                embed.set_image(url = "attachment://dhbc.png")
+                send = await ctx.send(
+                    embed = embed,
+                    file=discord.File('dhbc.png'))
+                if "a" in random_dhbc:
+                    message = await wait_message(send)
+                    if str(message.content.lower()) == dapan:
+                        await ctx.send(f'bạn đã trả lời đúng, đáp án là: {dapan}')
+                    elif str(message.content.lower()) == "gợi ý":
+                        if money >= 50:
                             await main.update(ctx.author.id, 50, "lose_wallet")
-                            await ctx.send(f'gợi ý, từ này là: {question["dataGame"]["suggestions"]}')
-                            message = await self.bot.wait_for('message', check = check, timeout=45)
-                            if str(message.content).lower() == str(question['dataGame']['tukhoa']).lower():
-                                await ctx.send(f'bạn đã trả lời đúng, đáp án là: "{question["dataGame"]["tukhoa"]}"')
-                            else:
-                                await ctx.send(f"sai rồi:(, đáp án đúng là: {question['dataGame']['tukhoa']}")
+                            await ctx.send(f"Gợi ý từ này là {sokt} (tiếp tục reply câu hỏi ở trên để trả lời)")
+                        else:
+                            await ctx.send("bạn không đủ 50$ để xem gợi ý(tiếp tục reply câu hỏi ở trên để trả lời)")
+                        message = await wait_message(send)
+                        if message.content.lower() == dapan:
+                            await ctx.send(f'bạn đã trả lời đúng, đáp án là: {dapan}')
+                        else:
+                            await ctx.send(f'sai rồi, đáp án là {dapan}')
                     else:
-                        if str(message.content).lower() == str(question['dataGame']['tukhoa']).lower():
-                            await ctx.send(f"Bạn đã trả lời đúng, đáp án là {question['dataGame']['tukhoa']}")
-                        else:
-                            await ctx.send(f"sai rồi:(, đáp án đúng là: {question['dataGame']['tukhoa']}")
-                if get == api[1]:
-                    get = await session.get(question['result']['link'])
-                    em = discord.Embed(title="_==== Đuổi Hình Bắt Chữ====_", description='đây là câu hỏi của bạn:\nreply tin nhắn này để trả lời câu hỏi và reply "gợi ý" để lấy gợi ý câu trả lời (50$/lần)', color = random.choice(list_color))
-                    file = discord.File(io.BytesIO(await get.read()), filename="dhbc.png")
-                    em.set_image(url = "attachment://dhbc.png")
-                    send = await ctx.send(embed=em, file = file)
-                    def check(m):
-                        return m.author == ctx.author and m.channel == ctx.channel and m.reference is not None and m.reference.message_id == send.id
-                    message = await self.bot.wait_for('message', check = check, timeout=45)
-                    if str(message.content).lower() == "gợi ý":
-                        
-                        if int(money) < 50:
-                            await ctx.send('bạn không đủ 50$ trong ví để xem gợi ý')
-                            message = await self.bot.wait_for('message', check = check, timeout=45)
-                            if str(message.content).lower() == str(question['result']['tukhoa']).lower():
-                                await ctx.send(f'bạn đã trả lời đúng, đáp án là: "{question["result"]["tukhoa"]}"')
-                            else:
-                                await ctx.send(f"sai rồi:(, đáp án đúng là: {question['result']['tukhoa']}")
-                        else:
-                            await main.update(ctx.author.id, 50, "lose_wallet")
-                            await ctx.send(f'gợi ý, từ này là: {question["result"]["suggestions"]}')
-                            message = await self.bot.wait_for('message', check = check, timeout=45)
-                            if str(message.content).lower() == str(question['result']['tukhoa']).lower():
-                                await ctx.send(f'bạn đã trả lời đúng, đáp án là: "{question["result"]["tukhoa"]}"')
-                            else:
-                                await ctx.send(f"sai rồi:(, đáp án đúng là: {question['result']['tukhoa']}")
+                        await ctx.send(f'sai rồi, đáp án là {dapan}')
+            elif random_dhbc == url_DHBC[1]:
+                json_dhbc = json_DHBC["data"]
+                img = json_dhbc["image1and2"]
+                answer = json_dhbc["wordcomplete"]
+                goi_y = json_dhbc["soluongkt"]
+                async with aiohttp.ClientSession() as session:
+                    img = await session.get(img)
+                    img = await img.read()
+                    f = await aiofiles.open("dhbc.png", mode="wb")
+                    await f.write(img)
+                    await f.close()
+                embed.set_image(url = "attachment://dhbc.png")
+                send = await ctx.send(embed=embed,file=discord.File('dhbc.png'))
+                message = await wait_message(send)
+                if message.content.upper() == answer:
+                    await ctx.send(f'bạn đã trả lời đúng, đáp án là: {answer}')
+                elif message.content.lower() == "gợi ý":
+                    if money >= 50:
+                        await main.update(ctx.author.id, 50, "lose_wallet")
+                        await ctx.send(f"Gợi ý: từ này gồm {goi_y} kí tự (tiếp tục reply câu hỏi ở trên để trả lời)")
                     else:
-                        if str(message.content).lower() == str(question['result']['tukhoa']):
-                            await ctx.send(f"Bạn đã trả lời đúng, đáp án là {question['result']['tukhoa']}")
-                        else:
-                            await ctx.send(f"sai rồi:(, đáp án đúng là: {question['result']['tukhoa']}")
-                if get == api[2]:
-                    get = await session.get(question['image1and2'])
-                    em = discord.Embed(title="_==== Đuổi Hình Bắt Chữ====_", description='đây là câu hỏi của bạn:\nreply tin nhắn này để trả lời câu hỏi', color = random.choice(list_color))
-                    file = discord.File(io.BytesIO(await get.read()), filename="dhbc.png")
-                    em.set_image(url = "attachment://dhbc.png")
-                    send = await ctx.send(embed=em, file = file)
-                    def check(m):
-                        return m.author == ctx.author and m.channel == ctx.channel and m.reference is not None and m.reference.message_id == send.id
-                    message = await self.bot.wait_for('message', check = check, timeout=45)
-                    if 1 == 2:
-                        print(9)
+                        await ctx.send("bạn không đủ 50$ để xem gợi ý(tiếp tục reply câu hỏi ở trên để trả lời)")
+                    message = await wait_message(send)
+                    if message.content.upper() == answer:
+                        await ctx.send(f"bạn đã trả lời đúng, đáp án là: {answer}")
                     else:
-                        if str(message.content).lower() == str(question['wordcomplete']).lower():
-                            await ctx.send(f"Bạn đã trả lời đúng, đáp án là {question['wordcomplete']}")
-                        else:
-                            await ctx.send(f"sai rồi:(, đáp án đúng là: {question['wordcomplete']}")
+                        await ctx.send(f"sai rồi, đáp án là {answer}")
+                else:
+                    await ctx.send(f"sai rồi, đáp án là {answer}")
+                    
         except Exception as e:
             print(e)
-            await ctx.send('error')
+            await ctx.send(
+                'hiện tại lệnh bạn đang sử dụng đã gặp lỗi, hãy thử lại sau. xin lỗi vì sự cố này'
+            )
 async def setup(bot):
-    await bot.add_cog(Dhbc(bot))
+    await bot))
