295e599ceef444143f2f64b9fc01615f18405b8c
CCcutcanh <91305214+CCcutcanh@users.noreply.github.com>
2023-01-25 15:29:59 +0700

    Update dog_coffee.py

--- command/dog_coffee.py
+++ command/dog_coffee.py
@@ -4,6 +4,7 @@ import easy_pil
 import aiofiles
 import aiohttp
 import asyncio
+import os
 class Coffee_dog(commands.Cog):
     config = {
         "name": "dog_coffee",
@@ -20,32 +21,32 @@ class Coffee_dog(commands.Cog):
                 image = await easy_pil.load_image_async(ctx.author.display_avatar.url)
                 async with aiohttp.ClientSession() as session:
                     async with session.get(f'https://i.ibb.co/T0XqWkt/paste.jpg') as resp:
-                        f = await aiofiles.open('paste.jpg', mode='wb')
+                        f = await aiofiles.open(os.path.dirname(__file__)+ '/cache/dogcoffee_background.jpg', mode='wb')
                         await f.write(await resp.read())
                         await f.close()
-                back = easy_pil.Editor('paste.jpg')
+                back = easy_pil.Editor(os.path.dirname(__file__)+ '/cache/dogcoffee_background.jpg')
                 paste = easy_pil.Editor(image).circle_image()
                 paste.resize((220, 220))
                 back.paste(paste, (305, 85))
-                file = discord.File(fp=back.image_bytes, filename='circle.png')
+                file = discord.File(fp=back.image_bytes, filename='dog_coffee.png')
                 async with ctx.typing():
-                    await asyncio.sleep(4)
+                    await asyncio.sleep(1)
                 await ctx.send(f"Khoẻ ko em:)) <@{ctx.message.author.id}>",file=file)
             else:
                 image = await easy_pil.load_image_async(member.display_avatar.url)
                 async with aiohttp.ClientSession() as session:
                     async with session.get(f'https://i.ibb.co/T0XqWkt/paste.jpg') as resp:
-                        f = await aiofiles.open('paste.jpg', mode='wb')
+                        f = await aiofiles.open(os.path.dirname(__file__)+ '/cache/dogcoffee_background.jpg', mode='wb')
                         await f.write(await resp.read())
                         await f.close()
-                back = easy_pil.Editor('paste.jpg')
+                back = easy_pil.Editor(os.path.dirname(__file__)+ '/cache/dogcoffee_background.jpg')
                 paste = easy_pil.Editor(image).circle_image()
                 paste.resize((220, 220))
                 back.paste(paste, (305, 85))
-                file = discord.File(fp=back.image_bytes, filename='circle.png')
+                file = discord.File(fp=back.image_bytes, filename='dog_coffee.png')
                 async with ctx.typing():
-                    await asyncio.sleep(4)
-                await ctx.send('Khoẻ ko em:)) <@{member.id}>',file=file)
+                    await asyncio.sleep(1)
+                await ctx.send(f"Khoẻ ko em:)) <@{ctx.message.author.id}>",file=file)
         except Exception as e:
             print(e)
 async def setup(bot):
