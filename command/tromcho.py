f9168d005d40190ff407e6d160ddcea63c4ac6d3
CCcutcanh <91305214+CCcutcanh@users.noreply.github.com>
2023-01-26 12:48:24 +0700

    Update tromcho.py

--- command/tromcho.py
+++ command/tromcho.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import easy_pil
 import aiofiles
 import aiohttp
+import os 
 class Tromcho(commands.Cog):
     config = {
         "name": "tromcho",
@@ -18,21 +19,22 @@ class Tromcho(commands.Cog):
             await ctx.send('error')
             return
         try:
-            image = await easy_pil.load_image_async(ctx.message.author.display_avatar.url)
-            image2 = await easy_pil.load_image_async(arg.display_avatar.url)
-            async with aiohttp.ClientSession() as session:
-                async with session.get(f'https://i.ibb.co/bgz3FYt/trom-cho-bi-xu-phat-the-nao-1811142901.jpg') as resp:
-                    f = await aiofiles.open('paste.jpg', mode='wb')
-                    await f.write(await resp.read())
-                    await f.close()
-            back = easy_pil.Editor('paste.jpg')
-            paste = easy_pil.Editor(image).circle_image()
-            paste2 = easy_pil.Editor(image2).circle_image()
-            paste2.resize((42, 42))
-            paste.resize((40, 40))
-            back.paste(paste, (279, 35))
-            back.paste(paste2, (210, 200))
-            await ctx.send(f't xich m lai bh:smiling_imp: :smiling_imp:{arg.mention}', file = discord.File(back.image_bytes, filename='circle.png'))
+            async with ctx.typing():
+                image = await easy_pil.load_image_async(ctx.message.author.display_avatar.url)
+                image2 = await easy_pil.load_image_async(arg.display_avatar.url)
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(f'https://i.ibb.co/bgz3FYt/trom-cho-bi-xu-phat-the-nao-1811142901.jpg') as resp:
+                        f = await aiofiles.open(os.path.dirname(__file__) + "/cache/" + 'tromcho_background.jpg', mode='wb')
+                        await f.write(await resp.read())
+                        await f.close()
+                back = easy_pil.Editor(os.path.dirname(__file__)+'/cache/tromcho_background.jpg')
+                paste = easy_pil.Editor(image).circle_image()
+                paste2 = easy_pil.Editor(image2).circle_image()
+                paste2.resize((42, 42))
+                paste.resize((40, 40))
+                back.paste(paste, (279, 35))
+                back.paste(paste2, (210, 200))
+                await ctx.send(f't xich m lai bay gio:smiling_imp: :smiling_imp:{arg.mention}', file = discord.File(back.image_bytes, filename='circle.png'))
         except Exception as e:
             print(e)
             await ctx.send('error')
