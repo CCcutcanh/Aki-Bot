e3dbeadf00afbd484721b82868f5c80bf37ebfd7
CCcutcanh <91305214+CCcutcanh@users.noreply.github.com>
2023-02-12 19:24:54 +0700

    Update vuatiengviet.py

--- command/vuatiengviet.py
+++ command/vuatiengviet.py
@@ -6,6 +6,7 @@ import random
 import asyncio
 import aiofiles
 import os
+import easy_pil
 class Vuatiengviet(commands.Cog):
     config = {
         "name": "vuatiengviet",
@@ -20,33 +21,38 @@ class Vuatiengviet(commands.Cog):
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get('https://raw.githubusercontent.com/undertheseanlp/dictionary/master/dictionary/words.txt') as word:
-                    vn_dict = []
+                    list_word = []
                     word = await word.text()
                     word = word.split('\n')
                     for i in word:
                         try:
                             text = json.loads(i)['text']
                             if len(text.split(' ')) == 2:
-                                vn_dict.append(text)
+                                list_word.append(text)
                         except:
                             continue
-                    random_word = random.choice(vn_dict)
-                async with session.get(f'https://docs-api.jrtxtracy.repl.co/vuatiengviet/image?word={random_word}') as resp:
-                    f = await aiofiles.open(os.path.dirname(__file__) + '/cache/vuatiengviet.png', mode='wb')
-                    await f.write(await resp.read())
-                    await f.close()
-                    send = await ctx.send('====Vua Tieng Viet====\nday la cau hoi cua ban:\nreply tin nhan nay de tra loi, ban co 45 giay de tra loi', file = discord.File(os.path.dirname(__file__) +'/cache/vuatiengviet.png'))
+                    word = random.choice(list_word).strip('-')
+                    print(word)
+                    text = ""
+                    question = list(word.replace(" ", ""))
+                    random.shuffle(question)
+                    for i in question:
+                        text += i+" /"
+                    img = easy_pil.Editor(os.path.dirname(__file__)+'/cache/'+"vuatiengviet_background.jpg")
+                    img.resize((1080,832))
+                    img.text(position = (540, 680), text = text,font = easy_pil.font.Font.montserrat(variant='bold',size=30),align="center")
+                    send = await ctx.send('đây là câu hỏi của bạn\nreply tin nhắn này để trả lời câu hỏi, bạn có 45 giây để trả lời', file = discord.File(img.image_bytes, filename='vuatiengviet.png'))
                     def check(m):
                         return m.author.id == ctx.author.id and m.channel == ctx.channel and m.reference is not None and m.reference.message_id == send.id
                     try:
-                        message = await self.bot.wait_for('message', timeout=45, check=check)
-                        if message:
-                            if str(message.content).lower() == str(random_word).lower():
-                                await ctx.send(f'bạn đã trả lời đúng, đáp án là "{random_word}"')
+                        message = await self.bot.wait_for("message", timeout=45, check=check)
+                        if message: 
+                            if str(message.content).lower() == word.lower():
+                                await ctx.send(f"ban da tra loi dung, dap an la: '{word}'")
                             else:
-                                await ctx.send(f'sai rồi đáp án là "{random_word}"')
+                                await ctx.send(f'sai roi, dap an la "{word}"')
                     except asyncio.TimeoutError:
-                        await ctx.send('Hết giờ!')
+                        await ctx.send("hết giờ!")
         except Exception as e:
             print(e)
 async def setup(bot):
